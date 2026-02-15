a0197d8b0fd6ee181369c6a5d4a34356e9f91af1
ymatuhin <ym@ymatuhin.ru>
2026-02-15 23:05:58 +0300

    add heading anchors, view transition and scroll padding/behavior

---
+++ public/assets/js/heading-anchors.js
@@ -0,0 +1,38 @@
+class HeadingAnchors extends HTMLElement {
+  static register(tagName) {
+    if ('customElements' in window) {
+      customElements.define(tagName || 'heading-anchors', HeadingAnchors);
+    }
+  }
+
+  connectedCallback() {
+    this.headings.forEach((heading) => {
+      if (!heading.hasAttribute('tabindex')) {
+        heading.setAttribute('tabindex', -1);
+      }
+      heading.insertAdjacentHTML(this.position, this.anchor(heading));
+    });
+  }
+
+  anchor(heading) {
+    let anchor = document.createElement('a');
+    anchor.href = `#${heading.id}`;
+    anchor.innerHTML = `Jump to '${heading.textContent}'`;
+
+    return anchor.outerHTML;
+  }
+
+  get headings() {
+    return this.querySelectorAll(this.selector);
+  }
+
+  get selector() {
+    return this.getAttribute('selector') || 'h2[id], h3[id], h4[id]';
+  }
+
+  get position() {
+    return this.getAttribute('position') || 'afterend';
+  }
+}
+
+HeadingAnchors.register();
