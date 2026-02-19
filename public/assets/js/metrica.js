11c0427c9d571a7a8664edf2ae61572a1544c3e7
ymatuhin <ym@ymatuhin.ru>
2026-02-19 12:19:45 +0300

    suppress failed fetch

--- public/assets/js/metrica.js
+++ public/assets/js/metrica.js
@@ -1,3 +1,3 @@
-window.addEventListener('load', function() {
-  fetch('https://mc.yandex.ru/watch/28017147');
+window.addEventListener('load', function () {
+  fetch('https://mc.yandex.ru/watch/28017147').catch(() => {});
 });
