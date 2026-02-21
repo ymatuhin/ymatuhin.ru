b12815e742a27547242a5d91679ca3cf19402294
ymatuhin <ym@ymatuhin.ru>
2026-02-22 01:06:17 +0300

    fix metrika

--- public/assets/js/metrica.js
+++
@@ -1,9 +0,0 @@
-window.addEventListener('load', function () {
-  const img = document.createElement('img');
-  img.src = 'https://mc.yandex.ru/watch/28017147';
-  img.alt = '';
-  img.style.position = 'absolute';
-  img.style.left = '-9999px';
-
-  document.body.appendChild(img);
-});
